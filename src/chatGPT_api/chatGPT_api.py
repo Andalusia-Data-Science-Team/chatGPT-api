7c03675255f6179aa22210136496fcda1d25f4d8
Ahmed badr <ahmed.k.badr.97@gmail.com>
2023-04-09 20:53:15 +0200

    handle api connection error add retry 5 times every 5 seconds

--- src/chatGPT_api/chatGPT_api.py
+++ src/chatGPT_api/chatGPT_api.py
@@ -1,13 +1,14 @@
 import time
 
 import openai
+from openai.error import APIConnectionError
 
 
 class ChatGPT:
 
     def __init__(self, api_key, model="gpt-3.5-turbo"):
         self._api_key = api_key
-        openai.api_key=api_key
+        openai.api_key = api_key
         self._message_history = []
         self._model = model
         self.total_tokens = 0
@@ -21,10 +22,21 @@ class ChatGPT:
         :return: message (str), reply status (str)
         """
         self._message_history.append({"role": 'user', "content": message})
+        try_again = True
+        retry_count = 5
+        try_count = 1
+        while try_again:
+            try:
+                try_again = False
+                completion = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=self._message_history)
+            except APIConnectionError:
+                if try_count <= retry_count:
+                    try_again = True
+                    print(f"api connection error {try_count} trying after 5 sec..")
+                    time.sleep(5)
 
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=self._message_history)
         reply_message = completion.choices[0].message.content
         reply_status = completion.choices[0].finish_reason
 
@@ -42,9 +54,9 @@ class ChatGPT:
         print("History cleared")
 
     def last_reply(self):
-        summary_text=f"last_message:{self._last_reply_message}\n{'-' * 10}\nreply_status:{self._last_reply_status}\n{'-' * 10}\ntotal_tokens:{self.total_tokens}"
+        summary_text = f"last_message:{self._last_reply_message}\n{'-' * 10}\nreply_status:{self._last_reply_status}\n{'-' * 10}\ntotal_tokens:{self.total_tokens}"
         print(summary_text)
-        return self._last_reply_message,self._last_reply_status
+        return self._last_reply_message, self._last_reply_status
 
     def chat_history(self):
         for message_dict in self._message_history:
